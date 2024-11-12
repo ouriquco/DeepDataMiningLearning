92d71bda87734445000d3582b3e6a0ed022dfa1c
Cody William Ourique <010892622@coe-hpc1.sjsuad.sjsu.edu>
2024-11-11 17:36:39 -0800

    modified conditional branch for choosing model architecture in models.py

--- DeepDataMiningLearning/detection/models.py
+++ DeepDataMiningLearning/detection/models.py
@@ -232,7 +232,7 @@ def create_detectionmodel(modelname, num_classes=None, trainable_layers=0, ckpt_
         names = modelname.split('_')
         
         if names[0] == 'customrcnn' and names[1] == 'convnext' and names[2] in opts and names[-1] == 'weights':
-            backbone_name = ' '.join(names[2], names[3])
+            backbone_name = ' '.join([names[2], names[3]])
             model=CustomRCNN(backbone_modulename=backbone_name,trainable_layers=trainable_layers,num_classes=num_classes,out_channels=256,min_size=800,max_size=1333)
             if ckpt_file:
                 model = load_checkpoint(model, ckpt_file, fp16)
@@ -243,7 +243,15 @@ def create_detectionmodel(modelname, num_classes=None, trainable_layers=0, ckpt_
                 model = load_checkpoint(model, ckpt_file, fp16)
         else:
             print("Model name not supported")
-
+    elif modelname.startswith('customrcnn'):
+        x = modelname.split("_")
+        if x[0]== 'customrcnn' and x[1].startswith('resnet'):
+            backbonename = x[1]
+            model=CustomRCNN(backbone_modulename=backbonename,trainable_layers=trainable_layers,num_classes=num_classes,out_channels=256,min_size=800,max_size=1333)
+            if ckpt_file:
+                model = load_checkpoint(model, ckpt_file, fp16)
+        else:
+            print("Model name not supported")
     ## End of added code
     else:
         print('Model name not supported')
